069d4aea8ef7cccf15afbba20296c857e7d281b2
BrickBoxNAFO <Jonathanleesiphone@gmail.com>
2026-04-07 21:26:29 +0200

    Include pdfkit font data for Vercel serverless deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [],
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+    images: {
+          domains: [],
+    },
+    typescript: {
+          ignoreBuildErrors: true,
+    },
+    eslint: {
+          ignoreDuringBuilds: true,
+    },
+    outputFileTracingIncludes: {
+          '/api/generate-certificate': ['./node_modules/pdfkit/js/data/**/*'],
+    },
 }
 
 module.exports = nextConfig
